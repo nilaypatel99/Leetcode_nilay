3082e1e8fb183baead8eb03ea66c7e0a74633339
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2025-01-04 13:18:08 +0530

    Time: 221 ms (74.94%) | Memory: 18.4 MB (22.86%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,21 +1,18 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        #Brute Force: we can use three for loops to get the unique palindrome 
-        #optimized: iterate through the each char and make a left and right sect and check if the char in left is 
-        #present in right,we use map for right and set for left to store the two halfs
-        #better optimized: we can add the current char to left and remove the char found in right
-
-        res=set()  #to store unique palindrome just the left and right
-        left=set()
-        right=Counter(s)
-
-        for m in s:     #check the current
-            right[m]-=1    #remove from right
-            
-            for char in left:
-                if right[char]>0:
-                    res.add((char,m))
-            left.add(m)
-        return len(res)
-
-
+        first_occurence=[float('inf')]*26
+        last_occurence=[0]*26
+        res=0
+        
+        # first and last occurrence of each character
+        for i,ch in enumerate(s):
+            idx=ord(ch)-ord('a')
+            first_occurence[idx]=min(first_occurence[idx],i)
+            last_occurence[idx]=i
+        
+        #number of unique char between the first and last char
+        for i in range(26):
+            if first_occurence[i]<last_occurence[i]:
+                unique_char=set(s[first_occurence[i]+1 : last_occurence[i]])       
+                res+=len(unique_char)
+        return res
