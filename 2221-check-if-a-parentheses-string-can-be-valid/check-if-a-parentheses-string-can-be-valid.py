c832abb7512d6c587b6ec0a2f06edb12ab72683f
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2025-01-13 11:34:08 +0530

    Time: 109 ms (46.84%) | Memory: 18.7 MB (38.79%) - LeetSync

--- 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.py
@@ -1,29 +1,30 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
+        #O(1) space complexity
+        #two variables and two traversals for knowing the valid parenthesis
+        #if the open_cnt and close_cnt should be positive
         
-        if len(s)%2!=0:   #as only even length pairs can form valid-parentheses('()')
-            return False 
-        
-        #we'll need two stack one to monitor open and no phase change and second for phase change
-        open_stack=[]
-        open_cls=[]
+        if len(s)%2!=0:
+            return False
 
-        for i in range(len(s)):
-            if locked[i]=='0':   #for push phase change parentheses
-                open_cls.append(i)
-            elif s[i]=='(':      #push no change values
-                open_stack.append(i)
-            elif s[i]==')':
-                if open_stack:
-                    open_stack.pop()
-                elif open_cls:
-                    open_cls.pop()
-                else:
-                    return False
+        open_cnt=0
+        cls_cnt=0
 
-        #this for right-side check close parentheses and pop
-        while open_stack and open_cls and open_stack[-1]<open_cls[-1]:
-            open_stack.pop()
-            open_cls.pop()
+        for i in range(len(s)):#LtoR
+            if s[i]=='(' or locked[i]=='0':
+                open_cnt+=1
+            else:
+                open_cnt-=1
+            
+            if open_cnt<0:
+                return False
         
-        return not open_stack
+        for j in range(len(s)-1,-1,-1):  #RtoL
+            if s[j]==')' or locked[j]=='0':
+                cls_cnt+=1
+            else:
+                cls_cnt-=1
+            if cls_cnt<0:
+                return False
+        
+        return True
