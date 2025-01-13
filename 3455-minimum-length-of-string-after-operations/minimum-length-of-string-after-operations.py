ea3eefa01b4facae3b401e9b49e4c66d8d28c11a
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2025-01-13 15:29:08 +0530

    Time: 139 ms (54.92%) | Memory: 18.8 MB (32.70%) - LeetSync

--- 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
+++ 3455-minimum-length-of-string-after-operations/minimum-length-of-string-after-operations.py
@@ -3,7 +3,7 @@ class Solution:
         freq=Counter(s)
 
         res=0
-        for ch,val in freq.items():
+        for val in freq.values():
             if val%2==0:  #if even no. of freq counts  (a..a..a..a=>a..a)
                 res+=2
             else:         #if odd no. of freq counts    (a..a..a=>a)
