b0e5454423f41539397044dc63a508539ff8b612
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2025-01-15 21:19:50 +0530

    Time: 547 ms (100.00%) | Memory: 0B (100.00%) - LeetSync

--- 3767-find-students-who-improved/find-students-who-improved.sql
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -1,17 +1,10 @@
-WITH cte AS (
-    SELECT
-        student_id,
-        subject,
-        MIN(exam_date) AS first_exam_date,
-        MAX(exam_date) AS latest_exam_date,
-        COUNT(DISTINCT exam_date) AS date_count
-    FROM Scores
-    GROUP BY student_id, subject
-    HAVING COUNT(DISTINCT exam_date) > 1
+WITH cte AS (SELECT student_id,subject,MIN(exam_date) AS fd,MAX(exam_date) AS ld
+FROM Scores
+GROUP BY student_id, subject
+HAVING COUNT(DISTINCT exam_date) > 1)
     -- ^ ensures the student took that subject on at least two different dates
-)
-SELECT
-    c.student_id,
+
+SELECT c.student_id,
     c.subject,
     s_first.score AS first_score,
     s_last.score  AS latest_score
@@ -19,10 +12,10 @@ FROM cte c
 JOIN Scores s_first
   ON s_first.student_id = c.student_id
  AND s_first.subject    = c.subject
- AND s_first.exam_date  = c.first_exam_date
+ AND s_first.exam_date  = c.fd
 JOIN Scores s_last
   ON s_last.student_id = c.student_id
  AND s_last.subject    = c.subject
- AND s_last.exam_date  = c.latest_exam_date
+ AND s_last.exam_date  = c.ld
 WHERE s_last.score > s_first.score
 ORDER BY c.student_id, c.subject;
