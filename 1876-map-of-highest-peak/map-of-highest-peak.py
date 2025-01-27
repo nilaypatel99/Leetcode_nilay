149c49a8a50a718b637a9bd3599b423c3b2dacd6
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2025-01-27 17:27:23 +0530

    Time: 697 ms (75.42%) | Memory: 84.9 MB (53.27%) - LeetSync

--- 1876-map-of-highest-peak/map-of-highest-peak.py
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -1,31 +1,29 @@
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        #The absolute diff should be one and the we want to maximize the height
+        #the diff should be one from all sides and how to do this
+        r,c=len(isWater),len(isWater[0])
+        res=[[-1]*c for _ in range(r)]
+        directions = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+        
+        queue=deque()
+        for i in range(r):
+            for j in range(c):
+                if isWater[i][j]==1:
+                    res[i][j]=0
+                    queue.append((i,j))
 
-        m, n = len(isWater), len(isWater[0])
         
-        # Initialize the height array with -1 for land, and 0 for water
-        height = [[-1]*n for _ in range(m)]
-        q = deque()
-        
-        # Step 1: Put all water cells in the queue with height = 0
-        for i in range(m):
-            for j in range(n):
-                if isWater[i][j] == 1:
-                    height[i][j] = 0
-                    q.append((i, j))
-        
-        # Directions: up, down, left, right
-        directions = [(0,1),(0,-1),(1,0),(-1,0)]
-        
-        # Step 2: BFS
-        while q:
-            r, c = q.popleft()
-            
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < m and 0 <= nc < n and height[nr][nc] == -1:
-                    height[nr][nc] = height[r][c] + 1
-                    q.append((nr, nc))
-        
-        return height
+        while queue:
+            i,j=queue.popleft()
+
+            for dir1 in directions:
+                i_,j_= i+dir1[0],j+dir1[1]
+
+                if 0<=i_<r and 0<=j_<c and res[i_][j_]==-1:
+                    res[i_][j_]=res[i][j]+1
+                    queue.append((i_,j_))
+
+        return res
+
 
