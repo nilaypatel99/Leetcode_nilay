5dbd3f0fe3b07c7bff3b434bd2ef154daf03815f
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2025-12-24 08:41:25 +0530

    Time: 4 ms (4.40%) | Memory: 17.4 MB (96.18%) - LeetSync

--- 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
+++ 3334-apple-redistribution-into-boxes/apple-redistribution-into-boxes.py
@@ -3,12 +3,11 @@ class Solution:
         #total capacity will always be greater than total apples
         tot_apple=sum(apple)
         tot_cap=sum(capacity)
-        res,i=0,0
+        i=0
 
         while tot_apple>0:
             my_list=sorted(capacity,reverse=True)
             tot_apple-=my_list[i]
-            res+=1
             i+=1
 
-        return res
+        return i
