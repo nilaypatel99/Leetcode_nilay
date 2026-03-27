fb9ff48ea5d8b67d99b6092a2135b6cbe3421df6
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2026-03-27 10:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (66.47%) - LeetSync

--- 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -1,24 +1,10 @@
-from collections import deque
-
 class Solution:
     def areSimilar(self, mat: list[list[int]], k: int) -> bool:
         n = len(mat[0])
-
-        shift_amount = k % n
-        
-        if shift_amount == 0:
-            return True
-            
-        shifted = []
-        for i, r in enumerate(mat):
-            d = deque(r)
-            if i % 2 == 0:
-                # Even row: Left shift
-                d.rotate(-shift_amount)
-            else:
-                # Odd row: Right shift
-                d.rotate(shift_amount)
-            shifted.append(list(d))
-            
-        return shifted == mat
-
+        k %= n
+        for r in mat:
+            # If a row shifted by k is equal to itself, it doesn't matter 
+            # if it's left or right; it just needs to be periodic.
+            if r != r[k:] + r[:k]:
+                return False
+        return True
