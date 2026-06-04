888e328555ad9d685e98ad35a260b8ecd8b68486
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2026-06-04 23:01:56 +0530

    Time: 251 ms (80.08%) | Memory: 19.3 MB (32.95%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -1,54 +1,20 @@
 class Solution:
+    def waviness(self, x: int) -> int:
+        s = str(x)
+
+        cnt = 0
+
+        for i in range(1, len(s) - 1):
+            if ((s[i] > s[i - 1] and s[i] > s[i + 1]) or
+                (s[i] < s[i - 1] and s[i] < s[i + 1])):
+                cnt += 1
+
+        return cnt
+
     def totalWaviness(self, num1: int, num2: int) -> int:
-        from functools import lru_cache
+        ans = 0
 
-        def F(x: int) -> int:
-            if x < 0:
-                return 0
+        for x in range(num1, num2 + 1):
+            ans += self.waviness(x)
 
-            digits = list(map(int, str(x)))
-            n = len(digits)
-
-            @lru_cache(None)
-            def dfs(pos, tight, started, prev2, prev1):
-                if pos == n:
-                    return (1, 0)  # (count, waviness_sum)
-
-                limit = digits[pos] if tight else 9
-
-                total_cnt = 0
-                total_sum = 0
-
-                for d in range(limit + 1):
-                    ntight = tight and (d == digits[pos])
-
-                    if not started and d == 0:
-                        c, s = dfs(pos + 1, ntight, False, -1, -1)
-
-                    elif prev1 == -1:
-                        # first real digit
-                        c, s = dfs(pos + 1, ntight, True, -1, d)
-
-                    elif prev2 == -1:
-                        # second real digit
-                        c, s = dfs(pos + 1, ntight, True, prev1, d)
-
-                    else:
-                        extra = int(
-                            (prev1 > prev2 and prev1 > d)
-                            or
-                            (prev1 < prev2 and prev1 < d)
-                        )
-
-                        c, s = dfs(pos + 1, ntight, True, prev1, d)
-
-                        s += extra * c
-
-                    total_cnt += c
-                    total_sum += s
-
-                return (total_cnt, total_sum)
-
-            return dfs(0, True, False, -1, -1)[1]
-
-        return F(num2) - F(num1 - 1)
+        return ans
