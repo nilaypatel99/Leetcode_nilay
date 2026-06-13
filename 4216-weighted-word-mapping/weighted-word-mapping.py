c135023898b43f4bc8660c25442ac69de8058892
nilaypatel <126392313+nilaypatel99@users.noreply.github.com>
2026-06-13 14:30:34 +0530

    Time: 4 ms (91.63%) | Memory: 19.1 MB (94.26%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        #pseudo code
+        #iterate through each word and calculate it's res
+        #for each res letter map it back using the 26-index_pos to get the backward pos
+        #join all the letters
+        res=[]
+        for word in words:
+            word_weight=0
+            for ch in word:
+                idx=ord(ch)-ord('a')
+                word_weight+=weights[idx]
+            mod=word_weight%26
+            mapped_char=chr(ord('z')-mod)
+            res.append(mapped_char)
+        return ''.join(res)
